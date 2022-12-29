4eb69f7f93a37fdccc39e6e5c9703c0a4e59b4d8
Alex Robillard, PhD <52674718+RobillardA@users.noreply.github.com>
2022-12-29 18:32:20 -0500

    Adjustments
    Deleted repeat code

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ wound_target = st.sidebar.text_input("Target number for wound roll", "4")
 wound_target = int(wound_target)
 
 # Create a slider to specify the number of dice for the hit roll
-num_hit_dice = st.sidebar.slider("Number of dice for hit roll", 1, 10, 3)
+num_hit_dice = st.sidebar.slider("Number of dice for hit roll", 1, 50, 3)
 
 # Create a dropdown menu to select the type of save
 save_types = [
@@ -72,27 +72,6 @@ for hit_rolls in combinations(range(1, num_sides+1), num_hit_dice):
             if sum(save_rolls) < save_target:
                 successful_saves += 1
 
-# Create a text input field to specify the number of sides on each die
-num_sides = st.sidebar.text_input("Number of sides on each die", "6")
-
-# Convert the number of sides to an integer
-num_sides = int(num_sides)
-
-# Create a text input field to specify the target number for the hit roll
-hit_target = st.sidebar.text_input("Target number for hit roll", "3")
-
-# Convert the hit target to an integer
-hit_target = int(hit_target)
-
-# Create a text input field to specify the target number for the wound roll
-wound_target = st.sidebar.text_input("Target number for wound roll", "4")
-
-# Convert the wound target to an integer
-wound_target = int(wound_target)
-
-# Create a slider to specify the number of dice for the hit roll
-num_hit_dice = st.sidebar.slider("Number of dice for hit roll", 1, 10, 3)
-
 # Create a dropdown menu to select the type of save
 save_types = [
     ("No save", 6),
@@ -105,42 +84,6 @@ selected_save = st.sidebar.selectbox("Select save type", [s[0] for s in save_typ
 for save, save_target in save_types:
   if save == selected_save:
     break
-
-# Create a text input field to specify the Armor Piercing (AP) save modifier
-ap_modifier = st.sidebar.text_input("Armor Piercing (AP) save modifier", "0")
-
-# Convert the AP modifier to an integer
-ap_modifier = int(ap_modifier)
-
-# Create a text input field to specify the damage done by the weapon
-damage = st.sidebar.text_input("Damage done by weapon", "1")
-
-# Convert the damage to an integer
-damage = int(damage)
-
-# Initialize counters for the number of successful hit rolls, wound rolls, and saves
-successful_hit_rolls = 0
-successful_wound_rolls = 0
-successful_saves = 0
-
-# Loop through all possible combinations of hit dice rolls
-for hit_rolls in combinations(range(1, num_sides+1), num_hit_dice):
-  # If the sum of the hit rolls is greater than or equal to the hit target,
-  # increment the successful hit rolls counter
-  if sum(hit_rolls) >= hit_target:
-    successful_hit_rolls += 1
-    # Loop through all possible combinations of wound dice rolls
-    for wound_rolls in combinations(range(1, num_sides+1), num_hit_dice):
-      # If the sum of the wound rolls is greater than or equal to the wound target,
-      # increment the successful wound rolls counter
-      if sum(wound_rolls) >= wound_target:
-        successful_wound_rolls += 1
-        # Loop through all possible combinations of save dice rolls
-        for save_rolls in combinations(range(1, num_sides+1), num_hit_dice):
-          # If the sum of the save rolls is less than the save target,
-          # increment the successful saves counter
-          if sum(save_rolls) < save_target:
-            successful_saves += 1
 
 # Calculate the probability of making a successful hit roll
 hit_probability = successful_hit_rolls / (num_sides ** num_hit_dice)
